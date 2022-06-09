85668fdfc7017196b6f82e4a50a2611d748d6760
H4ckint0sh <alijan.adeli@icloud.com>
2022-06-09 22:49:18 +0200

    removed floatterm

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -41,6 +41,11 @@ vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 local Terminal = require("toggleterm.terminal").Terminal
 
 local node = Terminal:new({ cmd = "node", hidden = true })
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _LAZYGIT_TOGGLE()
+	lazygit:toggle()
+end
 
 function _NODE_TOGGLE()
 	node:toggle()
