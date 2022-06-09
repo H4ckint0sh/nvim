3cbe773395b93c65a4e84d90f5917648473f5b03
H4ckint0sh <alijan.adeli@icloud.com>
2022-06-10 00:06:49 +0200

    added auto format on save to null-ls

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -1,29 +1,25 @@
-
 local status_ok, impatient = pcall(require, "copilot")
 if not status_ok then
-	  return
+	return
 end
-    vim.g.copilot_no_tab_map = true
-    vim.g.copilot_filetypes = {
-        ['*'] = false,
-        ['javascript'] = true,
-        ['javascriptreact'] = true,
-        ['typescript'] = true,
-        ['typescriptreact'] = true,
-        ['html'] = true,
-        ['css'] = true,
-        ['less'] = true,
-        ['scss'] = true,
-        ['lua'] = true,
-        ['rust'] = true,
-        ['c'] = true,
-        ['c#'] = true,
-        ['c++'] = true,
-        ['go'] = true,
-        ['python'] = true,
-        ['markdown'] = true,
-    }
-    vim.api.nvim_set_keymap('i', '<leader>ac', 'copilot#Accept("")', { silent = true, expr = true })
-
-
-
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_filetypes = {
+	["*"] = false,
+	["javascript"] = true,
+	["javascriptreact"] = true,
+	["typescript"] = true,
+	["typescriptreact"] = true,
+	["html"] = true,
+	["css"] = true,
+	["less"] = true,
+	["scss"] = true,
+	["lua"] = true,
+	["rust"] = true,
+	["c"] = true,
+	["c#"] = true,
+	["c++"] = true,
+	["go"] = true,
+	["python"] = true,
+	["markdown"] = true,
+}
+vim.api.nvim_set_keymap("i", "<leader>ac", 'copilot#Accept("")', { silent = true, expr = true })
