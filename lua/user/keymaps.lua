1cbdbb98fed0ba29ef8f2059c6fcf1feca7ed23a
H4ckint0sh <alijan.adeli@icloud.com>
2022-07-02 18:46:04 +0200

    moving scape map

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -34,15 +34,15 @@ if not vim.g.vscode then
 	-- Navigate buffers
 	keymap("n", "<S-l>", ":bnext<CR>", opts)
 	keymap("n", "<S-h>", ":bprevious<CR>", opts)
+	
+	-- Insert --
+	-- Press jk fast to enter
+	keymap("i", "jk", "<ESC>", opts)
 end
 
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
-
--- Insert --
--- Press jk fast to enter
-keymap("i", "jk", "<ESC>", opts)
 
 -- Visual --
 -- Stay in indent mode
