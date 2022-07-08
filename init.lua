5b846667a0ebf943ea90e278c2923219f8754258
H4ckint0sh <alijan.adeli@icloud.com>
2022-07-08 23:25:39 +0200

    adding outline buffers and renoving tabline

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ else
 	require("user.comment")
 	require("user.gitsigns")
 	require("user.nvim-tree")
-	require("user.bufferline")
+	require("user.outline")
 	require("user.lualine")
 	require("user.toggleterm")
 	require("user.project")
