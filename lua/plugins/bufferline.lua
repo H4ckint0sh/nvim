7a8efc584810eae219ba0229affdb254e3f99251
H4ckint0sh <alijan.adeli@icloud.com>
2024-07-01 23:35:59 +0200

    no seperator

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,9 +8,9 @@ bufferline.setup({
 		max_prefix_length = 8, -- prefix used when a buffer is de-duplicated
 		truncate_names = true, -- whether or not tab names should be truncated
 		tab_size = 10,
-		separator_style = { "", "" },
+		separator_style = {},
 		always_show_bufferline = true,
-		offsets = { { filetype = "NvimTree", text = "File Manager", text_align = "center", separator = true } },
+		offsets = { { filetype = "NvimTree", text = "File Manager", separator = false } },
 		style_preset = {
 			bufferline.style_preset.no_italic,
 			bufferline.style_preset.no_bold,
@@ -22,7 +22,7 @@ bufferline.setup({
 		show_close_icon = false,
 		show_tab_indicators = false,
 		indicator_icon = nil,
-		indicator = { style = "", icon = "" },
+		indicator = { style = "none", icon = "" },
 		buffer_close_icon = "",
 		modified_icon = "●",
 		close_icon = "",
