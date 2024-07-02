b8d73425715bc3773af85b5e279b83bd1410b1dc
H4ckint0sh <alijan.adeli@icloud.com>
2024-07-02 18:03:57 +0200

    adding auto tag rename plugin

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -652,6 +652,38 @@ return {
 		end,
 	},
 	{
+		"windwp/nvim-ts-autotag",
+		ft = {
+			"html",
+			"javascript",
+			"typescript",
+			"javascriptreact",
+			"typescriptreact",
+			"svelte",
+			"vue",
+			"tsx",
+			"jsx",
+			"rescript",
+			"xml",
+			"php",
+			"markdown",
+			"astro",
+			"glimmer",
+			"handlebars",
+			"hbs",
+		},
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = true, -- Auto close on trailing </
+				},
+			})
+		end,
+	},
+	{
 		"NvChad/nvim-colorizer.lua",
 		config = function()
 			require("plugins.colorizer")
