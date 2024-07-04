4c60b68fc9355a948783fec8dcb9b5abeec4a341
H4ckint0sh <alijan.adeli@icloud.com>
2024-07-05 00:41:10 +0200

    format on save and disable format commands globally or for the current buffer

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -18,6 +18,13 @@ conform.setup({
 		yaml = { "prettier" },
 		astro = { "prettier" },
 	},
+	format_on_save = function(bufnr)
+		-- Disable with a global or buffer-local variable
+		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+			return
+		end
+		return { timeout_ms = 500, lsp_format = "fallback" }
+	end,
 })
 
 vim.keymap.set({ "n" }, "<leader>f", function()
@@ -35,6 +42,24 @@ vim.keymap.set({ "v" }, "<leader>f", function()
 		timeout_ms = 500,
 	})
 end, { desc = "format selection" })
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
+})
 
 vim.api.nvim_create_user_command("Format", function(args)
 	local range = nil
